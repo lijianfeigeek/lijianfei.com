c21c22e2495acf04f28c97ec2528638b8bf0814d
lijianfei <lijianfei@sishuxuefu.com>
2019-04-21 17:07:53 +0800

    busuanzi

--- js/config.js
+++ js/config.js
@@ -5,14 +5,3 @@ window.AV.init({
     appId: APP_ID,
     appKey: APP_KEY
 });
-
-var analytics = AV.analytics({
-    appId: APP_ID,
-    appKey: APP_KEY,
-    // 你当前应用或者想要指定的版本号
-    version: '1.0.0',
-    // 你当前应用的渠道或者你想指定的渠道
-    channel: 'web',
-    // 选择服务地区（默认为国内节点）
-    region: 'cn'
-})
